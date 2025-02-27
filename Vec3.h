85260e757bd923d64c70dc14eb3571df40dd7a0f
Henry Shin <bg22915@bristol.ac.uk>
2025-02-27 19:09:56 +0000

    Vec3

---
+++ Vec3.h
@@ -0,0 +1,10 @@
+#ifndef CL_VEC3_H
+#define CL_VEC3_H
+
+#endif //CL_VEC3_H
+
+//#pragma once
+
+struct Vec3 {
+    float x, y, z;
+};
--- prac.cpp
+++ prac.cpp
@@ -1,1 +1,13 @@
-// 3차원 벡터 (Vec3) 연산(덧셈, 뺼셈, 내적, 외적)을 직접 구현해 보고, 테스트 코드 작성
+// 1교시: 3차원 벡터 (Vec3) 연산(덧셈, 뺼셈, 내적, 외적)을 직접 구현해 보고, 테스트 코드 작성
+
+#include "Vec3.h"
+#include "cmath"
+#include "iostream"
+
+Vec3 addVec3(const Vec3& a, const Vec3& b) {
+    return {a.x + b.x, a.y - b.y, a.z - b.z};
+}
+
+Vec3 subVec3(const Vec3& a, const VEc3& b) {
+    re
+}
